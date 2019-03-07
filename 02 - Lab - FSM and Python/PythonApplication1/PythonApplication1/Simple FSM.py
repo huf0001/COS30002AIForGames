cfd0c6da9087d1701a83e5f4f85581909e015332
huf0001 <samhuffer01@gmail.com>
2019-03-07 15:09:33 +1100

    Made the helloworld code, typed up the Simple FSM code in another .py file. I'll run that code in a moment. Oh, also moved the Python application I created in visual studio into the repo.

---
+++ 02 - Lab - FSM and Python/PythonApplication1/PythonApplication1/Simple FSM.py
@@ -0,0 +1,93 @@
+# variables
+ 
+fatigue = 0
+hunger = 0
+thirst = 0
+
+states = ["eating", "drinking", "sleeping", "awake"]
+currentState = "awake"
+
+alive = True
+running = True
+maxLimit = 100
+gameTime = 0
+
+while running and alive:
+    gameTime += 1
+
+    #sleeping: reduce fatigue, everything else increases
+    if currentState is "sleeping":
+        #sleep
+        print("Zzzzzzzzzzz")
+        fatigue -= 1
+        hunger += 1
+        thirst += 1
+
+        #check if not tired
+        if fatigue < 5:
+            currentState = "awake"
+
+    #awake: doing nothing, all variables increase
+    elif currentState is "awake":
+        #do nothing
+        print("Bored . . .")
+        hunger += 1
+        thirst += 1
+        fatigue += 1
+
+        #check for other states
+        if fatigue > 15:
+            currentState = "sleeping"
+        elif thirst > 7:
+            currentState = "drinking"
+        elif hunger > 7:
+            currentState = "eating"
+    
+    #eating: hunger reduces, fatigue and thirst increase
+    elif currentState is "eating":
+        #eat
+        print("Om nom nom nom")
+        hunger -= 1
+        thirst += 1
+        fatigue += 1
+
+        #check for other states
+        if fatigue > 15:
+            currentState = "sleeping"
+        elif thirst > 7:
+            currentState = "drinking"
+        elif hunger < 2:
+            currentState = "awake"
+    
+    #drinking: thirst reduces, all other states increase
+    elif currentState is "drinking":
+        #drink
+        print("Gulp gulp gulp")
+        thirst -= 1
+        hunger += 1
+        fatigue += 1
+
+        #check for other states
+        if fatigue > 15:
+            currentState = "sleeping"
+        elif thirst < 2:
+            if hunger > 7:
+                currentState = "eating"
+            else:
+                currentState = "awake"
+
+    else:
+        #broken
+        print("Why are you here? The code must have something wrong with it . . .")
+        die()
+
+    #checking if starved to death
+    if hunger > 20:
+        alive = False
+     
+    #checking for end of game time
+    if gameTime > maxLimit:
+        running = False
+
+print("--- The End ---")
+
