569d30c6203c48701cc21a392bd7e1c4f4b855ee
huf0001 <samhuffer01@gmail.com>
2019-03-15 22:12:31 +1100

    Started on the logic for AI2. Need to work out an error so that it only considers viable spaces that could create a 2 or 3 in a row in a row/column/diagonal that is not obstructed by a mark from AI1.

--- 03 - Lab - Tic Tac Toe/TicTacToe/TicTacToe/AI2.py
+++ 03 - Lab - Tic Tac Toe/TicTacToe/TicTacToe/AI2.py
@@ -2,57 +2,61 @@ import random
 
 class AI2:
     def MakeMove(self, boardSpaces, acceptableSpaces):
-        #acceptableSpaces = []
         result = None
-
-        #for i in range (0, len(boardSpaces)):
-        #    acceptableSpaces.append(i)
         
-        if len(acceptableSpaces) == 9:
-            temp = [0, 2, 6, 8]   
-            result = acceptableSpaces[temp[random.randrange(0, 3)]]
-        elif len(acceptableSpaces) > 1:
-            result = acceptableSpaces[random.randrange(0, len(acceptableSpaces) - 1)]
-        else:
-            result = acceptableSpaces[0]
+        if (len(acceptableSpaces) < 7):
+            result = self.CheckAttackableSpaces(boardSpaces, acceptableSpaces)
 
-        return result
+            if result is not None:
+                return result
 
+        return self.CheckOtherSpaces(acceptableSpaces)
     
-            # check for spaces that could block a 3 in a row. Only check if you cannot create a 3 in a row. These moves will also only appear for AI1 if 4 space are occupied (i.e. if both AIs have placed 2 marks)
-            #    else:
-            #        potentialAtk3 = False
-            #
-            #        for i in acceptableSpaces:
-            #            for j in possibleAtk3s:
-            #                for k in possibleAtk3s[j]:
-            #                    if not potentialAtk3:
-            #                        if possibleAtk3s[j][k] not in acceptableSpaces and boardSpaces[possibleAtk3s[j][k] - 1] is not "X":
-            #                            potentialAtk3 = True
-            
+    def CheckAttackableSpaces(self, boardSpaces, acceptableSpaces):
+        print ("AI2 checking attackable Spaces . . .")
+        atkSpaces = []
+        multiAtkSpaces = []
+        possibleAtks = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] 
+        possibleAtk = True
+        # check for spaces that could complete a 2 in a row.
+            # conditions:
+            #   there is a space marked by AI2 in a row/column/diagonal
+            #   there is an empty space in that row/diagonal/column (found by verifying that it is in acceptableSpaces)
+            #   AI1 has not marked a space in that row/column/diagonal
+        for i in range(len(acceptableSpaces)):
+            for j in range(len(possibleAtks)):
+                if acceptableSpaces[i] in possibleAtks[j]:
+                    for k in range(len(possibleAtks[j])):
+                        if boardSpaces[possibleAtks[j][k] - 1] is "X":
+                            possibleAtk = False
 
-        # check for spaces that could complete a 2 in a row. Such spaces can only appear for AI1 if 2 spaces are occupied (i.e. if both AIs have placed 1 mark)
-        #if len(acceptableSpaces) <= 7:
-        #    print()
+                    if possibleAtk is True:
+                        if acceptableSpaces[i] not in atkSpaces:
+                            atkSpaces.append(acceptableSpaces[i])
+                        elif acceptableSpaces[i] not in multiAtkSpaces:
+                            multiAtkSpaces.append(acceptableSpaces[i])
+                    else:
+                        possibleAtk = True
+        # spaces that are not viable attackable spaces are finding their way into atkSpaces and multiAtkSpaces. They shouldn't do that. Error in code would be above in the logic for selecting if a space gets added to atkSpaces
+        if len(multiAtkSpaces) > 0 and random.randrange(0, 1) > 0.5:
+            if len(multiAtkSpaces) is 1:
+                return multiAtkSpaces[0]
+            else:
+                return multiAtkSpaces[random.randrange(0, len(multiAtkSpaces) - 1)]
+        elif len(atkSpaces) > 0:
+            print("AI2 found attackable spaces")
+            if len(atkSpaces) is 1:
+                return atkSpaces[0]
+            else:
+                return atkSpaces[random.randrange(0, len(atkSpaces) - 1)]
+        else:
+            return None
 
-        # check for spaces that could block the opponent from completing a 3 in a row and make a 2 in a row
-        #if len(blockSpaces) > 0 and len(atk2Spaces) > 0:
-        #    for i in range(0, len(blockSpaces) - 1):
-        #        if blockSpaces[i] in atk2Spaces:
-        #            blockAndAtk2Spaces.append(blockSpaces[i])
-
-        # if len(blockAndAtk2Spaces) > 0:
-            # check for spaces that could block the opponent from completing a 3 in a row and make 2 2's in a row
-
-            # else pick a random space from blockAndAtk2Spaces
-
-        # else if len(blockSpaces) > 0:
-            # pick a random space from blockSpaces
-
-        # else if (atk2Spaces) > 0:
-            # pick a random space from atk2Spaces
-
-        # else
-
-        
-        # note: best move when the board is empty, or multiple block / atk2 / blockAndAtk2 spaces are available, is space no. 5 (the centre space)
+    def CheckOtherSpaces(self, acceptableSpaces):
+        if len(acceptableSpaces) == 9:
+            temp = [0, 2, 6, 8]                                                           # start in a corner space
+            return acceptableSpaces[temp[random.randrange(0, 3)]]
+        elif len(acceptableSpaces) > 1:
+            return acceptableSpaces[random.randrange(0, len(acceptableSpaces) - 1)]       # mark a random available space
+        else:
+            return acceptableSpaces[0]                                                    # mark the last space available
