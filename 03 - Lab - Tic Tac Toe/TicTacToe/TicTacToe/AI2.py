2411133bca6ed6f19af69501274eaede9681a93b
huf0001 <samhuffer01@gmail.com>
2019-03-15 15:52:49 +1100

    Cleaned up and organised AI1's logic, limiting it to either picking a random spot (the centre space if it is available), or picking a spot that will complete a 3 in a row. Comments for other logic have been moved to AI2; will develop it's logic next.

--- 03 - Lab - Tic Tac Toe/TicTacToe/TicTacToe/AI2.py
+++ 03 - Lab - Tic Tac Toe/TicTacToe/TicTacToe/AI2.py
@@ -17,3 +17,42 @@ class AI2:
             result = acceptableSpaces[0]
 
         return result
+
+    
+            # check for spaces that could block a 3 in a row. Only check if you cannot create a 3 in a row. These moves will also only appear for AI1 if 4 space are occupied (i.e. if both AIs have placed 2 marks)
+            #    else:
+            #        potentialAtk3 = False
+            #
+            #        for i in acceptableSpaces:
+            #            for j in possibleAtk3s:
+            #                for k in possibleAtk3s[j]:
+            #                    if not potentialAtk3:
+            #                        if possibleAtk3s[j][k] not in acceptableSpaces and boardSpaces[possibleAtk3s[j][k] - 1] is not "X":
+            #                            potentialAtk3 = True
+            
+
+        # check for spaces that could complete a 2 in a row. Such spaces can only appear for AI1 if 2 spaces are occupied (i.e. if both AIs have placed 1 mark)
+        #if len(acceptableSpaces) <= 7:
+        #    print()
+
+        # check for spaces that could block the opponent from completing a 3 in a row and make a 2 in a row
+        #if len(blockSpaces) > 0 and len(atk2Spaces) > 0:
+        #    for i in range(0, len(blockSpaces) - 1):
+        #        if blockSpaces[i] in atk2Spaces:
+        #            blockAndAtk2Spaces.append(blockSpaces[i])
+
+        # if len(blockAndAtk2Spaces) > 0:
+            # check for spaces that could block the opponent from completing a 3 in a row and make 2 2's in a row
+
+            # else pick a random space from blockAndAtk2Spaces
+
+        # else if len(blockSpaces) > 0:
+            # pick a random space from blockSpaces
+
+        # else if (atk2Spaces) > 0:
+            # pick a random space from atk2Spaces
+
+        # else
+
+        
+        # note: best move when the board is empty, or multiple block / atk2 / blockAndAtk2 spaces are available, is space no. 5 (the centre space)
