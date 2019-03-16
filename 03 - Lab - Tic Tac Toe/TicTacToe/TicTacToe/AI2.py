521420464563a5b6a5d2362089425baf413909c9
huf0001 <samhuffer01@gmail.com>
2019-03-16 21:20:08 +1100

    Found and fixed one last error with AI2's initial move. Also wrote up the submission doc for the task.

--- 03 - Lab - Tic Tac Toe/TicTacToe/TicTacToe/AI2.py
+++ 03 - Lab - Tic Tac Toe/TicTacToe/TicTacToe/AI2.py
@@ -14,7 +14,6 @@ class AI2:
     
     # find and choose an attackable space
     def CheckAttackableSpaces(self, boardSpaces, acceptableSpaces):
-        print ("AI2 checking attackable Spaces . . .")
         atkSpaces = []
         multiAtkSpaces = []
         possibleAtks = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] 
@@ -47,7 +46,6 @@ class AI2:
             else:
                 return multiAtkSpaces[random.randrange(0, len(multiAtkSpaces) - 1)]
         elif len(atkSpaces) > 0:                                            # otherwise pick any random attackable space
-            print("AI2 found attackable spaces")
             if len(atkSpaces) is 1:
                 return atkSpaces[0]
             else:
@@ -57,9 +55,9 @@ class AI2:
     
     # choosing a mostly random space
     def CheckOtherSpaces(self, acceptableSpaces):
-        if len(acceptableSpaces) >= 8:
-            temp = [0, 2, 6, 8]                                                           # start in a corner space
-            return acceptableSpaces[temp[random.randrange(0, 3)]]
+        if len(acceptableSpaces) > 7:
+            temp = [0, 2, 5, 7]                                                           # start in a corner space
+            return acceptableSpaces[temp[random.randrange(len(temp))]]
         elif len(acceptableSpaces) > 1:
             return acceptableSpaces[random.randrange(0, len(acceptableSpaces) - 1)]       # mark a random available space
         else:
